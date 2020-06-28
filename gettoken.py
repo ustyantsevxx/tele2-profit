3ca33a5f1d6fc20740275bf7530e7fac7336c6f0
tuist <tuistssip@gmail.com>
2020-06-28 15:15:28 +0500

    fix gettoken

--- gettoken.py
+++ gettoken.py
@@ -1,19 +1,38 @@
-import requests
+import asyncio
+from aiohttp import ClientSession
 from json import load, dump
+from colorama import init, Fore
 
-with open('config.json', 'r') as f:
-    NUMBER = load(f)['number']
+init()
 
-sms_post_url = f'https://tyumen.tele2.ru/api/validation/number/{NUMBER}'
-auth_post_url = 'https://my.tele2.ru/auth/realms/tele2-b2c/protocol/openid-connect/token'
-requests.post(sms_post_url, json={"sender": "Tele2"})
-code = input('sms code: ')
-response = requests.post(auth_post_url, {"client_id": "digital-suite-web-app",
+
+async def main():
+    with open('config.json', 'r') as f:
+        number = load(f)['number']
+
+    sms_post_url = f'https://tyumen.tele2.ru/api/validation/number/{number}'
+    auth_post_url = 'https://my.tele2.ru/auth/realms/tele2-b2c/protocol/openid-connect/token'
+
+    async with ClientSession() as sess:
+        print(Fore.LIGHTGREEN_EX + 'sending SMS...')
+        await sess.post(sms_post_url, json={"sender": "Tele2"})
+        code = input('SMS code: ')
+        print(Fore.CYAN + 'recieving token...')
+        response = await sess.post(auth_post_url,
+                                   data={"client_id": "digital-suite-web-app",
                                          "grant_type": "password",
-                                         "username": NUMBER,
+                                         "username": number,
                                          "password": code,
                                          "password_type": "sms_code"})
-TOKEN = response.json()['access_token']
+        token = (await response.json())['access_token']
+        print(Fore.GREEN + 'token recieved')
 
-with open('config.json', 'w') as f:
-    dump({'number': NUMBER, 'token': TOKEN}, f)
+    with open('config.json', 'w') as f:
+        dump({'number': number, 'token': token}, f)
+    print(Fore.Red + 'token saved to config.json')
+
+
+future = asyncio.ensure_future(main())
+asyncio.get_event_loop().run_until_complete(future)
+
+print(Fore.MAGENTA + 'Script terminated.')
