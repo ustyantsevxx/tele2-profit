c9748d4ce7c1053464bf69575f9207cac3f5c7cf
tuist <tuistssip@gmail.com>
2020-06-28 14:01:40 +0500

    async requests version

--- main.py
+++ main.py
@@ -1,61 +1,73 @@
-import requests
-from json import load
+import asyncio
+from aiohttp import ClientSession
+from json import load, dumps
+from colorama import init, Fore
+
+init()
 
 with open('config.json', 'r') as f:
     obj = load(f)
     NUMBER, TOKEN = obj['number'], obj['token']
-
-rs = requests.Session()
-rs.headers.update({'Authorization': f'Bearer {TOKEN}'})
 
 base_api = f'https://tyumen.tele2.ru/api/subscribers/{NUMBER}'
 market_api = f'{base_api}/exchange/lots/created'
 rests_api = f'{base_api}/siteTYUMEN/rests'
 
 
-def return_lot(lot_id):
-    return rs.delete(f'{market_api}/{lot_id}').json()
+async def return_lot(sess, lot_id):
+    print(Fore.CYAN + f'deleting {lot_id}...')
+    await sess.delete(f'{market_api}/{lot_id}')
+    print(Fore.GREEN + f'{lot_id} deleted')
 
 
-def sell_gb(amount, price):
-    return rs.put(market_api, json={
-        "trafficType": "data",
-        "cost": {"amount": price, "currency": "rub"},
-        "volume": {"value": amount, "uom": "gb"}
-    }).json()
-
-
-def sell_minutes(amount, price):
-    return rs.put(market_api, json={
+async def sell_minutes(sess, amount, price):
+    print(Fore.CYAN + f'listing {amount} for {price} rub...')
+    resp = await sess.put(market_api, json={
         "trafficType": "voice",
         "cost": {"amount": price, "currency": "rub"},
         "volume": {"value": amount, "uom": "min"}
-    }).json()
-
-
-def apply_emojes_and_shit(lot_id, lot_price):
-    rs.patch(f'{market_api}/{lot_id}', json={
-        "cost": {"amount": lot_price, "currency": "rub"},
+    })
+    print(Fore.GREEN + 'listed')
+    lot_id = (await resp.json())['data']['id']
+    print(Fore.YELLOW + f'patching {lot_id}...')
+    resp = await sess.patch(f'{market_api}/{lot_id}', json={
+        "cost": {"amount": price, "currency": "rub"},
         'showSellerName': True,
         "emojis": ["bomb", "devil", "scream"]
     })
+    print(Fore.BLUE + f'{lot_id} PATCHED!')
+    return await resp.json()
 
 
-def return_all():
-    lots = list(rs.get(market_api).json()['data'])
-    active_lots = [a['id'] for a in lots if a['status'] == 'active']
-    for lot in active_lots:
-        return_lot(lot)
+async def main():
+    tasks = []
+    headers = {'Authorization': f'Bearer {TOKEN}'}
+
+    async with ClientSession(headers=headers) as ses:
+        async with ses.get(market_api) as resp:
+            lots = list((await resp.json())['data'])
+        active_lots = [a['id'] for a in lots if a['status'] == 'active']
+
+        for lot_id in active_lots:
+            task = asyncio.ensure_future(return_lot(ses, lot_id))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
+        tasks = []
+
+        async with ses.get(rests_api) as resp:
+            rests = list((await resp.json())['data']['rests'])
+        minutes_total = sum([a['remain'] for a in rests if a['type'] == 'tariff'
+                             and a['uom'] == 'min'])
+
+        for i in range(int(minutes_total // 50)):
+            task = asyncio.ensure_future(sell_minutes(ses, 50, 40))
+            tasks.append(task)
+        resp = await asyncio.gather(*tasks)
+        print(dumps(resp, indent=2))
 
 
-def sell_all_minutes_by_50():
-    rests = list(rs.get(rests_api).json()['data']['rests'])
-    minutes_total = sum([a['remain'] for a in rests if a['type'] == 'tariff'
-                         and a['uom'] == 'min'])
-    for i in range(int(minutes_total // 50)):
-        lot = sell_minutes(50, 40)['data']
-        apply_emojes_and_shit(lot['id'], lot['cost']['amount'])
+future = asyncio.ensure_future(main())
+asyncio.get_event_loop().run_until_complete(future)
 
-
-return_all()
-sell_all_minutes_by_50()
+print(Fore.MAGENTA + 'Script terminated.')
